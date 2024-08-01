84b14b77ec95f2dc50acaf6f3bc84766e41ba270
prismtechbd3 <prismtechb@gmail.com>
2024-08-01 11:27:55 +0600

    Price Include Done

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -1,13 +1,1 @@
-<?php
 
-namespace App\Http\Controllers\User;
-
-use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
-use Inertia\Inertia;
-
-class UserController extends Controller
-{
-
-}
